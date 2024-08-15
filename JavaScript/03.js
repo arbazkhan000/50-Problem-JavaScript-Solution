42feedd0191e30c414eb2a816414cd5a772bd9d8
Arbazx01 <mdarbaz9675@gmail.com>
2024-08-15 11:55:30 +0530

    Secont Problem Solve

---
+++ JavaScript/03.js
@@ -0,0 +1,18 @@
+// funtion to add number and return the result
+
+// first method
+
+function addNumber(x, y) {
+    return x + y;
+}
+
+console.log(addNumber(7, 6));
+// output: 13
+
+// Second Method
+
+function addNumber(a, b) {
+    return a - b;
+}
+let result = addNumber(8 - 0);
+print(result);
